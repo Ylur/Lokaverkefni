ab2caf618fefd57b753c3fabeb068d196aee4fc6
Ingi Haralds <ingiharaldss@gmail.com>
2024-12-23 15:09:48 +0000

    "Add test endpoint to verify serverless functions"

--- bakendi/api/test.js
+++ bakendi/api/test.js
@@ -1,18 +1,5 @@
 // bakendi/api/test.js
 
-const cors = require('cors')({
-    origin: 'https://lokaverkefni-three.vercel.app', // Frontend URL
-    methods: ['GET'],
-    credentials: true,
-  });
-  
-  module.exports = async (req, res) => {
-    await cors(req, res, () => {
-      if (req.method === 'GET') {
-        res.status(200).json({ success: true, message: 'Test endpoint working' });
-      } else {
-        res.status(405).json({ success: false, error: 'Method Not Allowed' });
-      }
-    });
-  };
-  
+module.exports = (req, res) => {
+  res.status(200).json({ success: true, message: "API is working!" });
+};
