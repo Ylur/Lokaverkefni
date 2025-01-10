31bdb1a0e5df5aa46745be4537cf8947cff382fd
Ingi Haralds <ingiharaldss@gmail.com>
2025-01-10 11:39:48 +0000

    mainpage flex

--- Framendi/src/app/page.tsx
+++ Framendi/src/app/page.tsx
@@ -4,16 +4,31 @@ import React, { useContext } from "react";
 import "./globals.css";
 import Carousel from "./components/common/Carousel";
 import MiniOrderFlow from "./components/common/MiniOrderFlow";
+import Hero from "./components/common/Hero";
 
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col md:flex-row">
-      <div className="w-full md:w-2/3 p-4">
+    
+    <div className="flex flex-wrap md:flex-row">
+      <div className="w-3/4 p-4">
+        <Hero />
+      </div>
+      <div className="w-1/4 bg-gray-100 border-l-indigo-500">
+        <MiniOrderFlow />
+      </div>
+
+      <div className="w-2/4 p-4">
         <Carousel />
       </div>
-      <div className="w-full md:w-1/3 p-4 bg-gray-100">
-        <MiniOrderFlow />
+      <div className="w-2/4 p-4">
+        <Carousel />
+      </div>
+      <div className="w-2/4 p-4">
+        <Carousel />
+      </div>
+      <div className="w-2/4 p-4">
+        <Carousel />
       </div>
     </div>
   );
