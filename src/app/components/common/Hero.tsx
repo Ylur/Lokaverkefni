96b07aa67cd533074db66e1a696839a046773f55
Ingi Haralds <ingiharaldss@gmail.com>
2025-01-15 14:43:37 +0000

    blink blink

--- src/app/components/common/Hero.tsx
+++ src/app/components/common/Hero.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { FC } from "react";
+import "../../blink.css"; // blinking dot with restaurant color
 
 const Hero: FC = () => {
   return (
@@ -8,10 +9,16 @@ const Hero: FC = () => {
       className="relative bg-cover bg-center h-96"
       style={{ backgroundImage: 'url("/photos/lb.png")' }}
     >
-      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <div className="border rounded-lg absolute inset-0 bg-black opacity-50"></div>
       <div className="relative flex items-center justify-center h-full">
         <div className="text-center text-white">
-          <h1 className="text-5xl font-serif font-semibold">Our LiL Menu</h1>
+          {/* 
+            We replace the letter 'i' in "LiL" with 
+            L<span class="blink-i">i</span>L so we can control the dot
+          */}
+          <h1 className="text-5xl font-serif font-semibold">
+            L <span className="blink-i">l</span> L'B I T S
+          </h1>
         </div>
       </div>
     </div>
